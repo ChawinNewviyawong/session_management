e461390ed9eaad4602c393290de5e4125ce232ec
gear <N.Chawin@gmail.com>
2019-08-23 16:28:17 +0700

    gorilla

--- server.go
+++ server.go
@@ -1,26 +1,29 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
-	"github.com/alexedwards/scs"
-	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/memstore"
 	"github.com/gin-gonic/gin"
+	gorillaSessions "github.com/gorilla/sessions"
 )
 
-var sessionManager *scs.SessionManager
+var gorillastore = gorillaSessions.NewCookieStore([]byte("SESSION_KEY"))
 
 func main() {
 	router := gin.Default()
-	store := sessions.NewCookieStore([]byte("secret"))
+	store := memstore.NewStore([]byte("secret"))
 	router.Use(sessions.Sessions("mysession", store))
-
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "My Session Test",
 		})
 	})
+
 	router.POST("/signin", func(c *gin.Context) {
 		user := Login{}
 		fmt.Print(user.Username)
@@ -48,26 +51,89 @@ func main() {
 		// }
 		session := sessions.Default(c)
 		session.Set("sessionid", "user.Username")
-		session.Save()
+		// session.Save()
 		ssid := session.Get("sessionid")
 		c.JSON(http.StatusOK, gin.H{
 			"message":   "user.Username",
 			"sessionid": ssid,
 		})
 	})
-	router.GET("/show", func(c *gin.Context) {
-		session := sessions.Default(c)
-		ssid := session.Get("sessionid")
-		if ssid == "user.Username" {
-			c.JSON(http.StatusOK, gin.H{
-				"sessionid": ssid,
-				"message":   "my session",
-			})
-		} else {
+
+	router.GET("/gorillalogin", func(c *gin.Context) {
+		gorillasession, err := gorillastore.Get(c.Request, "sessionid")
+		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{
 				"error": http.StatusText(http.StatusUnauthorized),
 			})
+			return
 		}
+
+		gorillasession.Values["username"] = "username"
+		gorillasession.Save(c.Request, c.Writer)
+		c.JSON(http.StatusOK, gin.H{
+			"message": gorillasession.Values["username"],
+		})
+	})
+
+	router.POST("/getAllCars", func(c *gin.Context) {
+		session := sessions.Default(c)
+		sessionidserver := session.Get("sessionid")
+		var sessionidclient string
+		if err := c.ShouldBindJSON(&sessionidclient); err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": http.StatusText(http.StatusUnauthorized),
+			})
+		} else if sessionidserver != sessionidclient {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": http.StatusText(http.StatusUnauthorized),
+			})
+		} else {
+			// call ice's api
+			requestBody, _ := json.Marshal(map[string]string{
+				"profile": sessionidclient})
+			response, err := http.Post("", "application/json", bytes.NewBuffer(requestBody))
+			if err != nil || response.StatusCode != http.StatusOK {
+				c.JSON(http.StatusServiceUnavailable, gin.H{
+					"error": http.StatusText(http.StatusServiceUnavailable),
+				})
+			} else {
+				c.JSON(http.StatusOK, gin.H{
+					"cars": response.Body,
+				})
+			}
+		}
+
+	})
+	router.GET("/show", func(c *gin.Context) {
+		gorillasession, err := gorillastore.Get(c.Request, "sessionid")
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": http.StatusText(http.StatusUnauthorized),
+			})
+			return
+		}
+		fmt.Println(gorillasession.Values["username"])
+		if username, _ := gorillasession.Values["username"]; username == "username" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": http.StatusText(http.StatusUnauthorized),
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message": gorillasession.Values["username"],
+		})
+		// session := sessions.Default(c)
+		// ssid := session.Get("sessionid")
+		// if ssid == "user.Username" {
+		// 	c.JSON(http.StatusOK, gin.H{
+		// 		"sessionid": ssid,
+		// 		"message":   "my session",
+		// 	})
+		// } else {
+		// 	c.JSON(http.StatusUnauthorized, gin.H{
+		// 		"error": http.StatusText(http.StatusUnauthorized),
+		// 	})
+		// }
 	})
 	router.Run(":3000")
 }
